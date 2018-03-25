67c8ad22358b2414c0585a483a93041b2befb9fb
Bartol Freškura <freskura.bartol@gmail.com>
2018-03-25 15:15:23 +0200

    Enable test verbosity

--- src/metrics.py
+++ src/metrics.py
@@ -11,9 +11,9 @@ class Metrics(keras.callbacks.Callback):
     """
 
     def on_epoch_end(self, batch, logs={}):
-        print("\nEvaluating on the validation set")
+        print("\nEvaluating on the validation set...\n")
         yp = self.model.predict(self.validation_data[0],
-                                batch_size=self.params['batch_size'], verbose=0)
+                                batch_size=self.params['batch_size'], verbose=1)
 
         predict = np.argmax(yp, axis=1)
         targ = np.argmax(self.validation_data[1], axis=1)
@@ -22,8 +22,7 @@ class Metrics(keras.callbacks.Callback):
         prec = precision_score(targ, predict, average="macro")
         rec = recall_score(targ, predict, average="macro")
 
-        print("F1: {:.4f}, Precision: {:.4f}, Recall {:.4f}\n".format(
-            f1, prec, rec))
+        print("F1: {:.4f}, Precision: {:.4f}, Recall {:.4f}\n".format(f1, prec, rec))
 
 
 def top_3_acc(x, y, top_k=3):
